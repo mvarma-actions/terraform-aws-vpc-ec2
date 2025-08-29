d8a22cf2b38d47f7bb955cd395964ea0044bd915
mvarmasai <mvarma0293@gmail.com>
2025-08-28 23:11:22 -0400

    Add module to create 3 public and 3 private subnets across multiple AZs

---
+++ modules/subnets/main.tf
@@ -0,0 +1,32 @@
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+
+  vpc_id            = var.vpc_id
+  cidr_block        = var.public_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "tf-public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+
+  vpc_id            = var.vpc_id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "tf-private-subnet-${count.index + 1}"
+  }
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
